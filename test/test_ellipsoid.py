28da424c6818950106cc37fa567dfd65ca041394
eskorobogatov <egod09080908@yandex.ru>
2026-02-12 01:07:31 +0300

    started work on ellipsoid test

---
+++ test/test_ellipsoid.py
@@ -0,0 +1,16 @@
+import pytest
+
+from ransac3d.ransac_3d import RANSAC3D
+from ransac3d.surfaces.ellipsoid import Ellipsoid3D
+import numpy as np
+
+N_POINTS = 1000
+SEED = 42
+
+@pytest.fixture
+def ellipsoid_data():
+    pass
+
+@pytest.fixture
+def acceptable_rmse():
+    pass
