0bfa0430f71bf32c031e4c2fcd7aa254d318d8de
eskorobogatov <egod09080908@yandex.ru>
2026-03-03 21:10:00 +0300

    removed useless images

---
+++ examples/example_point.py
@@ -0,0 +1,25 @@
+import numpy as np
+from starkit_ransac.surfaces.point import Point3D
+from starkit_ransac.ransac_3d import RANSAC3D
+
+SEED = 42
+np.random.seed(SEED)
+
+def generate_data(real_point, max_noise_abs=0.5):
+    assert max_noise_abs >= 0
+    noise = np.random.random((100, 3)) * 2 - 1
+    noise = noise * max_noise_abs + real_point
+    real_and_noise = np.concat(([real_point], noise), axis=0)
+    return real_and_noise
+
+def main():
+    # generate synthetic data
+    data = generate_data(np.zeros(3))
+
+    ransac = RANSAC3D()
+    ransac.add_points(data)
+    fitted_model = ransac.fit(Point3D, 1000, 0.5)
+    print(fitted_model)
+
+if __name__ == "__main__":
+    main()
