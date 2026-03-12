fc23294ca6fb65e5b2e3c2da3b88235d59989220
eskorobogatov <egod09080908@yandex.ru>
2026-03-13 00:01:57 +0300

    added uniting function to visualize any shape

---
+++ starkit_ransac/visualisation/visualize.py
@@ -0,0 +1,42 @@
+import open3d as o3d
+from starkit_ransac.surfaces import (
+        Circle2D,
+        Line3D,
+        Plane3D,
+        Circle3D,
+        Sphere,
+        Ellipsoid3D,
+        MobiusStrip,
+        StepPlane
+)
+
+from starkit_ransac.visualisation import (
+    generate_circle2D_mesh,
+    generate_line3d_mesh,
+    generate_plane_mesh,
+    generate_circle_mesh,
+    generate_sphere_mesh,
+    generate_ellipsoid_mesh,
+    generate_mobius_mesh,
+    generate_stairs_mesh
+)
+
+TYPE_TO_GENERATOR = {
+        Circle2D : generate_circle2D_mesh,
+        Line3D : generate_line3d_mesh,
+        Plane3D : generate_plane_mesh,
+        Circle3D : generate_circle_mesh,
+        Sphere : generate_sphere_mesh,
+        Ellipsoid3D : generate_ellipsoid_mesh,
+        StepPlane : generate_stairs_mesh
+}
+
+def generate_mesh(
+        surface,
+        resolution=100,
+        color=(0, 1, 0)
+    ):
+    surface_type = type(surface)
+    func = TYPE_TO_GENERATOR[surface_type]
+    return func(surface, resolution=resolution, color=color)
+
